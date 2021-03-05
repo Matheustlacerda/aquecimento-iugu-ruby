cb9414160257e49a1f200b780aa4323d4ce938e8
Matheus_Lacerda <matheus_mtl@hotmail.com>
2021-03-05 14:04:17 -0300

    Erro na marcação do item conluido

--- study_item.rb
+++ study_item.rb
@@ -1,24 +1,29 @@
 class StudyItem
-  attr_reader :id, :title, :category
+  attr_reader :id, :title, :category, :status
 
   @@next_id = 1
   @@study_items = []
 
-  def initialize(title:, category:)
+  def initialize(title:, category:, status: false)
     @id = @@next_id
     @title = title
     @category = category
+    @status = status
 
     @@next_id += 1
-    @@study_items << self 
+    @@study_items << self
   end
 
   def include?(query)
     title.downcase.include?(query.downcase) || category.downcase.include?(query.downcase)
   end
 
+  def finished
+    @status = true
+  end
+
   def to_s
-    "##{id} - #{title} - #{category}" 
+    "##{id} - #{title} - #{category} [#{finished ? ' ' : 'X'}]"
   end
   
   def self.register
@@ -34,23 +39,28 @@ class StudyItem
     @@study_items    
   end
 
-  def self.search_items
+  def self.search_items    
     print 'Digite uma palavra para procurar: '
     term = gets.chomp
     found_items = all.filter do |item|
       item.include?(term)
     end
     puts found_items
-    puts "\nNenhum item encontrado" if found_items.empty?  #quando já tem um item não está mostrando essa informação
+    puts "\nNenhum item encontrado" if found_items.empty?
   end    
 
   def self.studied_items
-    puts '----------  Lista de itens  ----------'
+    puts '----------  Lista de itens de estudo  ----------'
     puts @@study_items
     puts
     puts 'Qual o id do item que já foi finalizado o estudo?'
     id = gets.to_i
-    study_item = all.detect { |item| item.id == id }
+    item = all.filter { |item| item.id == id }.first
+    if item.nil?
+      puts "\nId informado inválido"
+    else
+      item.finished
+      puts "\nStatus do item atualizado com sucesso"
     end
- 
+  end
 end
